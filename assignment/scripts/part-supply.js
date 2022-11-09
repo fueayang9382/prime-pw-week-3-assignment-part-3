b886d667dc9781bcd7bb7ad91f24285704b9f5fc
Tou Fue Yang <touyangbiz@gmail.com>
2022-11-09 16:37:22 -0600

    complete 3

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -4,13 +4,26 @@ console.log('****** Part Supply *******');
 // 1. Create a variable called 'partsNeeded' with a value of the number 40 
 //    & console.log the variable
 console.log('1. Number of partsNeeded:');
-
+console.log( '---1 my work starts here------------------------------------');
+let partNeeded = 40; 
+console.log( 'This is what we need for parts', partNeeded );
+console.log( '---1 my work ends here--------------------------------------');
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
+console.log( '---2 my work starts here-------------------------------------');
+
+let supplyChanges = [3, 5, -6, 0, 7, 11, ]
+console.log('this is our supply changes', supplyChanges);
+console.log( '---2 my work ends here---------------------------------------');
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
+console.log( '---3 my work starts here-------------------------------------');
+
+console.log('this is the second value of supplyChanges;', supplyChanges[2]);
+
+console.log( '---2 my work ends here---------------------------------------');
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
